f04a6be78d94098ba1cc02831d8033965c5d023c
akapucu <akapucu@kent.edu>
2017-05-10 16:04:14 -0500

    Update starter_script.sh

--- bin/starter_script.sh
+++ bin/starter_script.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
+#Python3.5 on redhat bug
+export LD_LIBRARY_PATH="/opt/rh/rh-python35/root/usr/lib64:${LD_LIBRARY_PATH}"
 
-cd $SPLUNK_HOME/etc/apps/TA-proofpoint_TAP/bin
+#cd $SPLUNK_HOME/etc/apps/TA-proofpoint_TAP/bin
 
-env -i /usr/bin/python3.5 $SPLUNK_HOME/etc/apps/TA-proofpoint_TAP/bin/PP_TAP_logs.py
+#env -i /usr/bin/python3.5 $SPLUNK_HOME/etc/apps/TA-proofpoint_TAP/bin/PP_TAP_logs.py
+cd /opt/splunk/etc/apps/TA-proofpoint_TAP/bin
+
+#env -i /usr/bin/python3.5 $SPLUNK_HOME/etc/apps/TA-proofpoint_TAP/bin/PP_TAP_logs.py
+#env -i /opt/rh/rh-python35/root/usr/bin/python3.5 $SPLUNK_HOME/etc/apps/TA-proofpoint_TAP/bin/PP_TAP_logs.py
+
+/opt/rh/rh-python35/root/usr/bin/python3.5 /opt/splunk/etc/apps/TA-proofpoint_TAP/bin/PP_TAP_logs.py
